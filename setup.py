8a9761cc227e553bedb7a743d120e9899ffb80be
Lars Kellogg-Stedman <lars@oddbit.com>
2010-01-30 13:31:00 -0500

    prep setup.py for cheesechop

--- setup.py
+++ setup.py
@@ -1,9 +1,57 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='dictConfigParser',
+setup(name='configdict',
         version='20091216.1',
-        description='Parse INI files into dictionaries.  Because, duh.',
+        description='Parse INI files into dictionaries.',
         author='Lars Kellogg-Stedman',
-        author_email='lars@seas.harvard.edu',
-        packages=['dictConfigParser'],
+        author_email='lars@oddbit.com',
+        url='http://github.com/larsks/configdict',
+        packages=['configdict'],
+        classifiers=[
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Operating System :: OS Independent',
+            ],
+        long_description='''\
+configdict: parse INI files into Python dictionaries
+====================================================
+
+``configdict`` parses INI-style configuration files into Python
+dictionaries.  Given a file like this::
+
+  [DEFAULT]
+
+  in stock = yes
+
+  [widgets]
+
+  price = 110.00
+  size = large
+
+  options = this \
+          that \
+          and \
+          the other thing
+
+You can do this::
+
+  >>> import configdict
+  >>> cf = configdict.ConfigDict('sample.conf')
+
+And get this::
+
+  >>> import pprint
+  >>> pprint.pprint(cf)
+  {'DEFAULT': {'in stock': 'yes'},
+   '__GLOBAL__': {},
+   'widgets': {'options': 'this that and the other thing',
+               'price': '110.00',
+               'size': 'large'}}
+
+And do this::
+
+   >>> print cf['widgets']['in stock']
+   yes
+
+'''
         )
+
